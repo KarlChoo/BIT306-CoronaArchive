3e9d485b79925799aa99fab7d93b709ed66fb838
DylanChng <47546918+DylanChng@users.noreply.github.com>
2020-10-07 21:26:56 +0800

    my forms for new tests

--- src/app/content/test/test.component.ts
+++ src/app/content/test/test.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PatientType } from "../../model/patientType.model";
+import { Symptoms } from "../../model/symptom.model";
 
 @Component({
   selector: 'app-test',
@@ -12,4 +14,20 @@ export class TestComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  patientType: PatientType[] = [
+    {value: 'returnee', viewValue: 'Returnee'},
+    {value: 'quarantined', viewValue: 'Quarantined'},
+    {value: 'close', viewValue: 'Close Contact'},
+    {value: 'infected', viewValue: 'Infected'},
+    {value: 'suspected', viewValue: 'Suspected'}
+  ];
+
+  symptom: Symptoms[] = [
+    {symptomName: 'Fever'},
+    {symptomName: 'Cough'},
+    {symptomName: 'Tiredness'},
+    {symptomName: 'Breathing Difficulty'},
+    {symptomName: 'Chest Pain'},
+    {symptomName: 'Loss of Speech or Movement'}
+  ];
 }
