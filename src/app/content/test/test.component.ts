5e6db726228bdcc708fd3f42a59981dcefb16bbb
DylanChng <47546918+DylanChng@users.noreply.github.com>
2020-10-08 01:19:40 +0800

    removal of checkbox

--- src/app/content/test/test.component.ts
+++ src/app/content/test/test.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { PatientType } from "../../model/patientType.model";
-import { Symptoms } from "../../model/symptom.model";
 
 @Component({
   selector: 'app-test',
@@ -22,12 +21,4 @@ export class TestComponent implements OnInit {
     {value: 'suspected', viewValue: 'Suspected'}
   ];
 
-  symptom: Symptoms[] = [
-    {symptomName: 'Fever'},
-    {symptomName: 'Cough'},
-    {symptomName: 'Tiredness'},
-    {symptomName: 'Breathing Difficulty'},
-    {symptomName: 'Chest Pain'},
-    {symptomName: 'Loss of Speech or Movement'}
-  ];
 }
--- src/app/model/symptom.model.ts
+++
@@ -1,3 +0,0 @@
-export interface Symptoms {
-  symptomName: string;
-}
