a2c825be88cf05ecf37e8368281c7c1da43e1796
DylanChng <47546918+DylanChng@users.noreply.github.com>
2020-10-08 17:42:52 +0800

    update test

--- src/app/content/test/test.component.ts
+++ src/app/content/test/test.component.ts
@@ -1,41 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { PatientType } from "../../model/patientType.model";
+import { Result } from "../../model/patientType.model";
 import { NgForm } from "@angular/forms";
 import { Test } from "../../model/Test.model";
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatSort } from '@angular/material/sort';
-import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent implements OnInit {
+export class TestComponent implements OnInit, AfterViewInit {
   constructor() {  }
   ngOnInit(): void {
   }
 
-  //drop down list
-  patientType: PatientType[] = [
-    {value: 'returnee', viewValue: 'Returnee'},
-    {value: 'quarantined', viewValue: 'Quarantined'},
-    {value: 'close', viewValue: 'Close Contact'},
-    {value: 'infected', viewValue: 'Infected'},
-    {value: 'suspected', viewValue: 'Suspected'}
-  ];
+  //tests array
+  testList: Test[] = [];
 
-    //sample data of tests
-  testList: Test[] = [
-      {testID: "T1",testDate: "08/10/2020", username: "Pete", patientType: "returnee", symptom: "Fever", status: "Completed"},
-      {testID: "T2",testDate: "08/10/2020", username: "Jackson", patientType: "suspected", symptom: "Fever", status: "Completed"},
-      {testID: "T3",testDate: "08/10/2020", username: "Bob", patientType: "close", symptom: "Cough", status: "Completed"},
-      {testID: "T4",testDate: "08/10/2020", username: "Anson", patientType: "quarantined", symptom: "Cough", status: "Pending"},
-  ];
   //bind with table
-  displayedColumns: string[] = ['testID', 'testDate', 'username', 'patientType','symptom','status','update'];
+  displayedColumns: string[] = ['testID', 'testDate', 'username', 'patientType','symptom','status','result','update'];
   dataSource = new MatTableDataSource<Test>(this.testList);
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   //method
   recordNewTest(formData: NgForm){
@@ -57,10 +46,64 @@ export class TestComponent implements OnInit {
         testDate: todayDate,
         username: formData.value.username,
         patientType: formData.value.patientType,
-        symptom: "Fever",
+        symptom: formData.value.symptom,
         status: "Pending"
       }
       this.testList.push(newTest);
+      console.log(this.testList);
       formData.resetForm();
+      this.refreshTable();
+      alert("Test ID:" + theID + " Successfully Added.");
   }
+
+  recordExistForm(formData: NgForm){
+    if (formData.invalid){
+      return;
+    }
+
+    //generate ID
+    var theLetter = "T";
+    var theLength = this.testList.length + 1;
+    var conv = theLength.toString();
+    var theID = theLetter.concat(conv);
+
+    //generate date
+    var todayDate = new Date().toLocaleString();
+
+    const newTest: Test = {
+      testID: theID,
+      testDate: todayDate,
+      username: formData.value.username,
+      patientType: formData.value.patientType,
+      symptom: formData.value.symptom,
+      status: "Pending"
+    }
+    this.testList.push(newTest);
+    console.log(this.testList);
+    formData.resetForm();
+    this.refreshTable();
+    alert("Test ID:" + theID + " Successfully Added.");
+  }
+    //drop down lists
+    patientType: PatientType[] = [
+      {value: 'returnee', viewValue: 'Returnee'},
+      {value: 'quarantined', viewValue: 'Quarantined'},
+      {value: 'close', viewValue: 'Close Contact'},
+      {value: 'infected', viewValue: 'Infected'},
+      {value: 'suspected', viewValue: 'Suspected'}
+    ];
+
+    result: Result[] = [
+      {value: true, viewValue: 'Positive'},
+      {value: false, viewValue: 'Negative'}
+    ];
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  refreshTable(){
+    this.dataSource.paginator = this.paginator;
+  }
+
  }
