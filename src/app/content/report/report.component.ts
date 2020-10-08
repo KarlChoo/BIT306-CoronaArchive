f6a4242f2d6aa2b941efc257a24a4954ae431f3c
Karl Choo Jia Long <karlchoo37@gmail.com>
2020-10-08 23:59:37 +0800

    report progress

--- src/app/content/report/report.component.ts
+++ src/app/content/report/report.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ReportService } from "./report.service";
+import { Test } from "../../model/Test.model";
 
 @Component({
   selector: 'app-report',
@@ -8,9 +9,22 @@ import { ReportService } from "./report.service";
 })
 export class ReportComponent implements OnInit {
 
-  constructor() { }
+  testList: Test[] = [];
+  breakpoint:number;
+  dimensionRatio;
 
-  ngOnInit(): void {
+  constructor(public reportService:ReportService) { }
+
+  ngOnInit() {
+    this.breakpoint = (window.innerWidth <= 1000) ? 1 : 3;
+    this.dimensionRatio = (window.innerWidth <= 1000) ? "2:0.5" : "2.5:1";
+    console.log(this.dimensionRatio);
+
+  }
+
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 1000) ? 1 : 3;
+    this.dimensionRatio = (window.innerWidth <= 1000) ? "2:0.5" : "3:1";
   }
 
 }
--- src/app/content/report/report.service.ts
+++ src/app/content/report/report.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Test } from "../../model/Test.model";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,31 @@ import { Injectable } from '@angular/core';
 export class ReportService {
 
   constructor() { }
+
+  private testList: Test[] = [
+    {testID: "T1",testDate: "08/10/2020", username: "Pete", patientType: "returnee", symptom: "Fever", status: "Completed"},
+    {testID: "T2",testDate: "08/10/2020", username: "Jackson", patientType: "suspected", symptom: "Fever", status: "Completed"},
+    {testID: "T3",testDate: "08/10/2020", username: "Bob", patientType: "close", symptom: "Cough", status: "Completed"},
+    {testID: "T4",testDate: "08/10/2020", username: "Anson", patientType: "quarantined", symptom: "Cough", status: "Pending"},
+  ];
+
+  private dailyCase:number = 56;
+  private monthlyPatients:number = 1289;
+  private kitCount:number = 468;
+
+  getAllTests(){
+    return this.testList;
+  }
+
+  getDailyCase(){
+    return this.dailyCase;
+  }
+
+  getMonthlyPatients(){
+    return this.monthlyPatients;
+  }
+
+  getKitCount(){
+    return this.kitCount;
+  }
 }
