af3d4c89104cd073b62783f0ba30d640323358ad
DylanChng <47546918+DylanChng@users.noreply.github.com>
2020-10-08 20:49:14 +0800

    update Tests

--- src/app/content/result/result.component.ts
+++ src/app/content/result/result.component.ts
@@ -3,16 +3,16 @@ import { MatTableDataSource } from '@angular/material/table';
 import { PatientResult } from "../../model/result.model";
 
 const ELEMENT_DATA: PatientResult[] = [
-  {testNo: 1, testID: 't0001', centreName: 'Puchong',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
-  {testNo: 2, testID: 't0002', centreName: 'Setapak',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
-  {testNo: 3, testID: 't0003', centreName: 'Setapak',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
-  {testNo: 4, testID: 't0004', centreName: 'Puchong',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
-  {testNo: 5, testID: 't0005', centreName: 'Setapak',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
-  {testNo: 6, testID: 't0006', centreName: 'SS2',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
-  {testNo: 7, testID: 't0007', centreName: 'Setapak',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
-  {testNo: 8, testID: 't0008', centreName: 'Puchong',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
-  {testNo: 9, testID: 't0009', centreName: 'SS2',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
-  {testNo: 10, testID: 't0010', centreName: 'SS2',  testDate: '01/01/2020', status: 'pending', result: 'N/A'},
+  {testNo: 1, testID: 'T1', centreName: 'Puchong',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
+  {testNo: 2, testID: 'T2', centreName: 'Setapak',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
+  {testNo: 3, testID: 'T3', centreName: 'Setapak',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
+  {testNo: 4, testID: 'T4', centreName: 'Puchong',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
+  {testNo: 5, testID: 'T5', centreName: 'Setapak',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
+  {testNo: 6, testID: 'T6', centreName: 'SS2',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
+  {testNo: 7, testID: 'T7', centreName: 'Setapak',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
+  {testNo: 8, testID: 'T8', centreName: 'Puchong',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
+  {testNo: 9, testID: 'T9', centreName: 'SS2',  testDate: '01/01/2020', status: 'completed', result: 'negative'},
+  {testNo: 10, testID: 'T10', centreName: 'SS2',  testDate: '01/01/2020', status: 'pending', result: 'N/A'},
 ];
 
 @Component({
