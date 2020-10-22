279ab0eaac47b4eebd6d310360f8a044f2ddb2c7
Karl Choo Jia Long <karlchoo37@gmail.com>
2020-10-23 01:21:18 +0800

    Before disaster

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//Models
+//const ;
 
 const app = express();
+
+mongoose.connect("mongodb+srv://zulul:TyA3dm8a94XADnUX@cluster0.wpsie.mongodb.net/coronarchive?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("Connected to database")
+  })
+  .catch(() => {
+    console.log("Connected failed to database")
+  })
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) =>{
   res.setHeader("Access-Control-Allow-Origin","*");
   res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, OPTIONS, PUT");
   next();
 })
 
