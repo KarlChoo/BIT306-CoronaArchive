65a009d14ca3f11cf7cfed754bcb1a20bfb3cc86
Karl Choo Jia Long <karlchoo37@gmail.com>
2020-11-24 03:51:55 +0800

    spacing

---
+++ backend/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator")
+
+const userSchema = mongoose.Schema({
+  username: {type: String, required: true},
+  password: {type: String, required: true},
+  name: {type: String, required: true},
+  position: {type: String, required: false},
+  patientType: {type: String, required: false},
+  symptoms: {type: String, required: false},
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User",userSchema); //Name of collection to insert,update and delete data from
