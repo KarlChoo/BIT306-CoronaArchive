cb2ea8c33045e1313d77fd20126d1abdfe0dcafa
Karl Choo Jia Long <karlchoo37@gmail.com>
2020-11-24 15:03:37 +0800

    unique centre name

--- backend/models/testcentre.js
+++ backend/models/testcentre.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator")
 
 //Defines the schema of the collection
 const testCentreSchema = mongoose.Schema({
   centreName : {type: String, required: true},
   centreManager: {type: String, required: true}
 });
+
+testCentreSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("TestCentre",testCentreSchema); //Name of collection to insert,update and delete data from
